8d51f816a47631f12c4591330019eb8a7a556ea1
Kotikov <11deadsluts@gmail.com>
2022-12-22 20:05:02 +0300

    first task is solved

--- hw17.py
+++ hw17.py
@@ -4,12 +4,17 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-a = input ('dd: ')
-sum = 0
-for symbol in a:
-    if symbol.isdigit()==True:
-        symbol = int (symbol)
-        sum = sum+symbol
-    else:
-        None
-print(sum)
+def sum_of_digits(user_number):
+    sum_up = 0
+    for symbol in user_number:
+        if symbol.isdigit()==True:
+            symbol = int (symbol)
+            sum_up = sum_up+symbol
+        else:
+            None
+    return sum_up
+
+print ("This program can sum up every digit in your input number")
+user_input = input ('Please input your number: ')
+print(sum_of_digits(user_input))
+
